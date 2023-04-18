044b4c0fe6fe99692c1629683bc783e8a29a2dd9
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-04-18 08:53:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,92 +1,34 @@
 
-// var Name = document.getElementById('name').value
-// const profession = document.getElementById('profession')
-// const age = document.getElementById('age')
+var nameInput = document.getElementById('name')
+var professionInput = document.getElementById('profession')
+var ageInput = document.getElementById('age')
 
-var delClicked = false;
+var button = document.getElementById('addUser')
 
-var addedEmployees = document.getElementById('addedEmployees')
+button.addEventListener('click',()=>{
+    var name = nameInput.value
+    var profession = professionInput.value
+    var age = ageInput.value
 
+    if(name == "" || profession == "" || age == ""){
+            document.querySelector('.status-msg').style.display = 'block'
+            document.querySelector('.status-msg').style.color = 'red'
+    }else{
+        document.getElementById('emp-li-container').innerHTML += `
+        <div class="emp-card" id="${name}-${profession}-${age}">
+            <div class="details">
+                <span>${name}</span>
+                <span>${profession}</span>
+                <span>${age}</span>
+            </div>
+            <button id="del-user-btn" onclick="deleteUser('${name}-${profession}-${age}')">Delete Employee</button>
+        </div>`
+    }
 
+})
 
-function addEmp(){
-  var Name = document.getElementById('name').value
-  
-  var profession = document.getElementById('profession').value
-  var last_line = document.getElementById('last-line')
- 
-  var age = document.getElementById('age').value
-  var table = document.createElement('table')
-
-  var trList = document.querySelectorAll('tr')
-  var trLength = trList.length;
-
-
-
-  
-
-  var p1 = document.createElement('p');
-
-  if(trLength > 0){
-    p1.remove
-  }
-
-  if(Name.trim() == "" && profession.trim() == "" && age.trim() == "" && trLength == 0){
-
-    p1.innerHTML = "Error : Please Make sure All fields are filled before adding in an employee !"
-    addedEmployees.before(p1)
-    p1.style.display = 'block';
-  }else {
-  
-
-  var tr = document.createElement('tr')
-  
-  var td1 = document.createElement('td')
-  var td2 = document.createElement('td')
-  var td3 = document.createElement('td')
-  var td4 = document.createElement('td')
-  var td5 = document.createElement('td')
-
-
-  td1.innerHTML = trLength+1;
-  td2.innerHTML = Name;
-  td3.innerHTML = profession;
-  td4.innerHTML = age;
-  td5.innerHTML = `<button type="button" id="del" onclick="del()">Delete User`
-
-
-
- 
-
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  tr.appendChild(td3);
-  tr.appendChild(td4);
-  tr.appendChild(td5);
-
-  table.appendChild(tr)
-  last_line.before(table)
-
-
-
-  
-delClicked = false;
-  var p2 = document.createElement('span')
- if(trLength == 1){
-
-  p2.innerHTML = "Success Employee added"
-  addedEmployees.before(p2)
-  p2.classList.add('green')
-
- }
-  
-
-  console.log(table.rows.length)
-
-}
-
-}
-
-function del(){
-  delClicked = true;
+function deleteUser(id){
+    
+    var Delete = document.getElementById(id)
+    document.querySelector('#emp-li-container').removeChild(Delete)
 }
